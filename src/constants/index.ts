446a7df066bc61f7e66ab987f3c28641dcf5fcb1
Olavo Mars <olavoamneves@gmail.com>
2024-02-20 18:08:41 -0400

    fixing color in-line style in constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const landingPage = {
   title:
-    'Acelere a sua <span class="text-primaryLightGreen">pesquisa</span> <br /> por empresas Brasileiras',
+    'Acelere a sua <span  style="color:#7DC590">pesquisa</span> <br /> por empresas Brasileiras',
   subtitle:
     'Poupe <u>suas horas e as da sua equipe</u> quando estiver buscando por empresas brasileiras e foque no que <b>realmente importa.</b>',
   ctaButton: 'Comece já',
